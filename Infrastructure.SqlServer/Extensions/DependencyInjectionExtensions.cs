e3463261bcb41fddf260fe7d1e189a2545e5d0fd
me <aszn.dev@gmail.com>
2024-07-22 20:49:52 +0500

    init

---
+++ Infrastructure.SqlServer/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.SqlServer.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration, string connectionString)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(connectionString)));
+            return services;
+        }
+    }
+}
---
+++ Mapster/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Infrastructure.SqlServer.Extensions;
+using Microsoft.Extensions.Configuration;
+
+namespace Mapster
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.local.json", optional: true)
+                .Build();
+
+            builder.Services.AddAppDbContext(config, "App");
+
+
+            using IHost host = builder.Build();
+
+            await host.RunAsync();
+        }
+    }
+}
