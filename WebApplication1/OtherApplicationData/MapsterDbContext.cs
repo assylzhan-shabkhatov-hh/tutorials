474d9569a9dfee1f3e004d7f2487795919543549
me <aszn.dev@gmail.com>
2024-07-26 08:30:17 +0500

    scaffold dbcontext

---
+++ WebApplication1/OtherApplicationData/MapsterDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.OtherApplicationData.Models;
+
+namespace WebApplication1.OtherApplicationData;
+
+public partial class MapsterDbContext : DbContext
+{
+    public MapsterDbContext()
+    {
+    }
+
+    public MapsterDbContext(DbContextOptions<MapsterDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:OtherApplication");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WebApplication1/OtherApplicationData/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.OtherApplicationData.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string SharedEmail { get; set; } = null!;
+}
