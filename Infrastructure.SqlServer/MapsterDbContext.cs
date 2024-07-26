498e8d175c86c5e859144a41019495f8c75e2390
me <aszn.dev@gmail.com>
2024-07-26 08:07:58 +0500

    init

---
+++ Infrastructure.SqlServer/MapsterDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SqlServer;
+
+public partial class MapsterDbContext : DbContext
+{
+    public MapsterDbContext()
+    {
+    }
+
+    public MapsterDbContext(DbContextOptions<MapsterDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:OtherApplication");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Mapster/Program.cs
+++ Mapster/Program.cs
@@ -19,4 +19,19 @@ var serviceProvider = new ServiceCollection()
         .AddAppDbContext(config, "Application")
         .BuildServiceProvider();
 
-var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+var person1 = new Person("Alice", 30);
+
+var person2 = new Person("Bob", 25);
+
+
+
+var personDict = new Dictionary<Person, string>
+{
+
+  { person1, "Some information about Alice" },
+
+  { person2, "Some information about Bob" }
+
+};
+var searchPerson = new Person("Alice", 30);
+Console.WriteLine(personDict.ContainsKey(searchPerson));
